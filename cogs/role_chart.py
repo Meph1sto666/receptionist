341a3bf821ae6a4982c96c671007bb9a20f32fe8
Meph1sto666 <phentragon@gmx.de>
2023-08-18 07:43:48 +0200

    unsquished role_chart labels

--- cogs/role_chart.py
+++ cogs/role_chart.py
@@ -15,16 +15,17 @@ class RoleChartCog(commands.Cog):
 	@commands.has_any_role(*getRoles(["tester"]))
 	async def listUserInvites(self, ctx:discord.Message) -> None:
 		roles:list[discord.Role]|None = None if ctx.guild == None else list(filter(lambda x: x.name != "@everyone", ctx.guild.roles))
-		_, ax = plt.subplots() # type: ignore
+		_, ax = plt.subplots(figsize=(10, 6)) # type: ignore
 		await ctx.guild.chunk() # type: ignore
 
 		if roles == None: raise Exception
 		data: list[int] = [len(r.members) for r in roles]
-		labels: list[str] = [f"[{roles[r].name}] {data[r]} / {round((data[r]/ctx.guild.member_count)*100),2}%" for r in range(len(roles))] # type: ignore
+		labels: list[str] = [f"[{roles[r].name}] {data[r]}" for r in range(len(roles))] # type: ignore
 		colors: list[str] = ['#{0:02x}{1:02x}{2:02x}'.format(*r.color.to_rgb()) for r in roles]
 
-		ax.pie([d/ctx.guild.member_count for d in data], labels=labels, colors=colors) # type: ignore
+		ax.pie([d/ctx.guild.member_count for d in data], colors=colors, autopct='%1.1f%%') # type: ignore
 		ax.set_title(f'Server Roles [{ctx.guild.member_count} users total]') # type: ignore
+		ax.legend(labels, loc="center right", bbox_to_anchor=(0, 0)) # type: ignore
 
 		fname:str = md5(b"".join([str(r.id).encode() for r in roles])).hexdigest()
 		plt.savefig(f"./data/temp/{fname}") # type: ignore
